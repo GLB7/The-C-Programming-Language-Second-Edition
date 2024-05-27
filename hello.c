6af9278a80f7c0fbfceeaba55cd8a18ba04bd2ba
Giovani Bergamasco <glb7@njit.edu>
2024-05-27 04:52:27 -0400

    Day 1 Page 13

---
+++ hello.c
@@ -0,0 +1,6 @@
+#include <stdio.h> // includes information about standard library.
+
+main(){ // main is a special function where execution begins for all programs. In this case main is a function taking no arguments with no specified return value.
+    printf("hello, world\n"); // calls the printf function from the stdio library with an argument specifying what to be printed.
+    // \n is an escape sequence that adds a new line to your text above. A full list of escape sequences is found in Section 2.3 which starts at page 37.
+}
